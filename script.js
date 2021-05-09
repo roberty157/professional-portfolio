dc037d54dd231cd1def7e541f589c702a2c4b700
Robert <robertyeam@gmail.com>
2021-05-08 21:49:36 -0700

    updated resume

---
+++ script.js
@@ -0,0 +1,13 @@
+let Recipidia = document.querySelector("#Recipidia");
+let Scheduler = document.querySelector("#Scheduler");
+let Dashboard = document.querySelector("#Dashboard");
+
+Recipidia.addEventListener("click",()=>{
+    window.open("https://roberty157.github.io/Recipidia/", '_blank');
+});
+Scheduler.addEventListener("click",()=>{
+    window.open("https://roberty157.github.io/work-day-scheduler/", '_blank');
+});
+Dashboard.addEventListener("click",()=>{
+    window.open("https://roberty157.github.io/weather-dashboard/", '_blank');
+});
